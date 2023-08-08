bc1ad76574cee41297686ea796fb9a0c1910df83
Soumajit-Pramanik <soumajit.pramanik@gmail.com>
2023-08-08 15:00:26 +0530

    Add files via upload

---
+++ KG/TRIPLE_SCORER/kgextract.py
@@ -0,0 +1,58 @@
+# this python file contains the functions needed to compute the question, triple similarity for UNIQORN
+import os
+import sys
+
+file_dir = os.path.dirname(__file__)
+sys.path.append(file_dir)
+print(file_dir)
+
+import BERT.config as config
+import copy
+import BERT.dataset as dataset
+import torch
+from collections import OrderedDict
+from BERT.model import BERTBaseCased
+import torch.nn as nn
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def getBERTmodel(bertdir):
+    MODEL = BERTBaseCased()
+    model_old = torch.load(bertdir, map_location=device)
+    new_state_dict = OrderedDict()
+    for k, v in model_old.items():
+        name = k[7:] # remove `module.`
+        new_state_dict[name] = v
+        # load params
+    MODEL.load_state_dict(new_state_dict)
+    MODEL.eval()
+    return MODEL
+
+def getsimilarity(q1,q2,model):
+    return sentence_pair_prediction(q1,q2,model)
+
+def sentence_pair_prediction(q1,q2,model):
+    tokenizer = config.TOKENIZER
+    max_len = config.MAX_LEN
+    q1 = str(q1)
+    q2 = str(q2)
+    inputs = tokenizer.encode_plus(q1,q2,add_special_tokens = True, max_length=max_len, padding='longest',truncation=True)
+    ids = inputs["input_ids"]
+    mask = inputs["attention_mask"]
+    token_type_ids = inputs["token_type_ids"]
+    padding_length = max_len - len(ids)
+    ids = ids + ([0] * padding_length)
+    mask = mask + ([0] * padding_length)
+    token_type_ids = token_type_ids + ([0] * padding_length)
+    #unsqueeze to make a batch of 1
+    ids = torch.tensor(ids,dtype=torch.long).unsqueeze(0)
+    mask = torch.tensor(mask, dtype = torch.long).unsqueeze(0)
+    token_type_ids = torch.tensor(token_type_ids, dtype = torch.long).unsqueeze(0)
+    #'targets' : torch.tensor(self.target[item], dtype=torch.long)}
+    ids = ids.to(device, dtype = torch.long)
+    token_type_ids = token_type_ids.to(device, dtype = torch.long)
+    mask = mask.to(device, dtype = torch.long)
+    #targets = targets.to(device, dtype = torch.float)
+    outputs = model(ids=ids, mask=mask, token_type_ids=token_type_ids)
+    outputs =  torch.sigmoid(outputs).cpu().detach().numpy()
+    return outputs[0][0]
+
---
+++ KG/TRIPLE_SCORER/scoreTriple_HYB.py
@@ -0,0 +1,116 @@
+#Extract triples in the format to be used by UNIQORN 
+# s -> p -> o -> Q
+# BERT is used to score the triple and they are written to file 
+import torch
+import pickle
+import re
+import kgextract as kex
+import sys
+import json
+import os
+import argparse
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+predicate_pattern   = re.compile('^P[0-9]*$')
+entity_pattern      = re.compile('^Q[0-9]*$')
+
+def read_kg(dirs):
+    verbfacts = []
+    with open(dirs, 'rb') as fp:
+        triples2 = pickle.load(fp)
+    return triples2
+
+def gettriplesforEntity(triples2, key):
+    #alltriples =  [triples2[t] for t in triples2]
+    verbfacts = []
+    flattened_list = triples2[key]
+    for xt in flattened_list:
+        #get all label
+        #broke = False
+        t = [{'id':itm['id'],'label': itm['label']} if itm['label']!='' and itm['label']!=[''] else  {'id':itm['id'],'label': itm['id']} for itm in xt]
+        tplabels = [x['label'] for  x in t]
+        tpids = [x['id'] for  x in t if entity_pattern.match(x['id'].strip())]
+        sp = t[0]['id']
+        pr = t[1]['id']
+        finallabel =[]
+        for x in tplabels:
+            if type(x) == list:
+                possibleitem = x[0].strip().strip('"').strip()
+                for item in x:
+                    #possibleitem = x[0].strip('"')
+                    if entity_pattern.match(item.strip()):
+                        continue
+                    elif predicate_pattern.match(item.strip()):
+                        continue
+                    else:
+                        possibleitem = item.strip().strip('"').strip()
+                        break
+                finallabel.append(possibleitem)
+            else:
+                finallabel.append(x.strip().strip('"').strip())
+
+        verbfacts.append(finallabel)
+    return verbfacts
+
+def getQuestion(qir):
+    questns = {}
+    for line in open(qir,'r'):
+        data = json.loads(line)
+        questns[data['id']] = {'text':data['question']}
+    return questns
+
+
+def main(argv):
+    questionfile = argv.inputfile
+    questionids =  getQuestion(questionfile)
+
+    print("Loading BERT!!!")
+    bertdir = argv.modeldir
+    model = kex.getBERTmodel(bertdir)
+    model.to(device)
+    print("BERT Loaded!!!")
+    
+    factdir = argv.triplefile
+    outputdir = argv.outputfile
+
+    for qid in list(questionids.keys()):
+        print(qid)
+        #pathx = '/GW/qa2/work/uniqorn/Crawl_Google_docs/LCQuAD_2/KGmode/hdt/Experiments/elqExperiments/NEDExperiments/ELQ_TAGME/ScoreTriples/spo_'+qid+'.txt'
+        ques = questionids[qid]['text']
+        dirs = factdir+'/SPO_'+qid+'.pkl'
+        try:
+            kg = read_kg(dirs)
+        except IOError:
+            #continue
+            pass
+        if len(kg) <= 0:
+            print("Text length not up content, Continue ....., actual length = ",len(kg))
+            #continue
+        f1 = open(outputdir+'/spo_'+qid+'.txt','w')
+        f2 = open(outputdir+'/tripscore_'+qid+'.txt','w')
+        ccnt=0
+        for keys in kg:
+            texts = gettriplesforEntity(kg, keys)
+            for tiples in texts:
+                tiples2 = ' ### '.join(tiples)
+                tripletoscore = ' '.join(tiples)
+                score = kex.getsimilarity(ques,tripletoscore,model)
+                f1.write('triple_' + str(ccnt) + ' ##### ' +  str(score) + ' #### ' + tiples2)
+                f1.write('\n')
+                if tiples[1].strip().lower()!='instance of' and tiples[1].strip().lower()!='occupation':
+                    f2.write('triple_'+str(ccnt) + '|' + 'triple_' + str(ccnt) + '|'+ str(score))
+                    f2.write('\n')
+                ccnt = ccnt + 1
+        f1.close()
+        
+if __name__ == "__main__":
+    # Create the parser
+    parser = argparse.ArgumentParser(description='Command Line argument for Aliases extraction from triples.')
+    # Add an argument
+    parser.add_argument('--inputfile', type=str, required=True,help='The json file containing the questions.')
+    parser.add_argument('--modeldir', type=str, required=True,help='BERT checkpoint to use for scoring')
+    parser.add_argument('--triplefile', type=str, required=True,help='The pkl file containing the triples directly from CLOCQ')
+    parser.add_argument('--outputfile', type=str, required=True,help='The directory to store the BERT scored triples for each question using CLOCQ.')
+    # Parse the argument
+    argv = parser.parse_args()
+    main(argv)
