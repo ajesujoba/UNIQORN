0de3068ca68c97f14e56bc9a0d0577673cf99c3b
Jesujoba Oluwadara Alabi <alabijesujoba@gmail.com>
2023-11-02 12:10:26 +0100

    Add files via upload

---
+++ TrainingScript/KGTEXT/create_posneg.sh
@@ -0,0 +1,5 @@
+python create_posneg.py \
+	--spanvalue 50 \
+	--trainfile /GW/qa2/work/uniqorn/Crawl_Google_docs/LCQuAD_2/LcQUAD2.0_data/train.json \
+        --textdir /GW/qa2/work/uniqorn/Crawl_Google_docs/LCQuAD_2/LcQUAD2.0_data/Data_Clean/LCQUAD2.0_ \
+	--outputfile posneg.csv 
